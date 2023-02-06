afb49d529b461b23c2c0bbf8449998e66ed2c81b
erben <erben@umich.edu>
2023-02-06 13:27:05 -0500

    update

---
+++ turkeyearthquakedonation/pages/donate.js
@@ -0,0 +1,109 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.headerStyle}>
+        <div className={styles.upperlinksStyle}>
+            <ul>
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/donate">Donate</Link></li>
+                <li><Link href="/learn">Learn</Link></li>
+            </ul> 
+        </div>
+     </div>
+      <main className={styles.main}>
+        
+        <div className={styles.description}>
+          <p>
+          <Link href="https://venmo.com/u/TurkishStudentAssociation">Donate now</Link>
+          </p>
+        </div>
+
+        <div className={styles.center}>
+          <h1>A list of trustwhorthy organizations for donations</h1>
+          
+        </div>
+        <div className={styles.grid}>
+        <a
+            href="https://venmo.com/u/TurkishStudentAssociation"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={inter.className}>
+            Turkish Student Association
+            </h2>
+            <p className={inter.className}>
+              As TSA at the University of Michigan, we are accepting donations on venmo as the fastest option to donate the money to the most trustwhorthy organizations.
+            </p>
+          </a>
+          <a
+            href="https://www.akut.org.tr/en/donation"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={inter.className}>
+            AKUT
+            </h2>
+            <p className={inter.className}>
+              A voluntary, non-governmental organization involved in searching, assisting, and rescuing the victims affected. They are fully transparent about the donations they receive and their spending. Accepts EUR and USD
+            </p>
+          </a>
+          <a
+            href="https://bagis.ahbap.org/bagis"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={inter.className}>
+            AHBAP
+            </h2>
+            <p className={inter.className}>
+              With 200 philanthropists and 30,000 volunteers, they are a  local voluntary network. Accepts credit card in Lira.
+            </p>
+          </a>
+          <a
+            href="https://twitter.com/TurkishEmbassy/status/1622614360190320640"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={inter.className}>
+            Turkish Embassy
+            </h2>
+            <p className={inter.className}>
+              The Turkish Embassy is accepting donations to their bank account. 
+            </p>
+          </a>
+          <a
+            href="https://afad.gov.tr/depremkampanyasi2"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={inter.className}>
+            AFAD
+            </h2>
+            <p className={inter.className}>
+              Turkey's official disaster and emergency management authority. Accepts EUR and USD
+            </p>
+          </a>
+
+        </div>
+      </main>
+    </>
+  )
+}
---
+++ turkeyearthquakedonation/pages/index.js
@@ -0,0 +1,96 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.headerStyle}>
+        <div className={styles.upperlinksStyle}>
+            <ul>
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/donate">Donate</Link></li>
+                <li><Link href="https://www.cnn.com/middleeast/live-news/turkey-earthquake-latest-020623/index.html">Learn</Link></li>
+            </ul> 
+        </div>
+     </div>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <p>
+            <Link href="https://venmo.com/u/TurkishStudentAssociation">Donate now</Link>
+          </p>
+
+        </div>
+
+        <div className={styles.center}>
+          <h1>Earthquake in Turkey</h1>
+          <p>On February 6th, Turkey woke up with devastating news by two massive earthquakes that affected over 10 cities. Death toll climbed to 1,651 in Turkey as of now and thousands of people are on the streets waiting to be rescued. It is currently snowing in some of these cities and we are in need of any amount of donation to help them survive.
+  </p>
+          
+        </div>
+        <div className={styles.images}>
+          <Image className={styles.card}
+           src="/turkiye1.jpeg"
+           width={300}
+           height={200}
+           />
+          <Image className={styles.card}
+           src="/turkiye2.jpeg"
+           width={300}
+           height={200}
+           />
+           <Image className={styles.card}
+           src="/turkiye3.jpeg"
+           width={300}
+           height={200}
+           />
+           <Image className={styles.card}
+           src="/turkiye4.jpeg"
+           width={300}
+           height={200}
+           />
+          
+        </div>
+        <div className={styles.grid}>
+          <a
+            href="/donate"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={inter.className}>
+              Donate <span>-&gt;</span>
+            </h2>
+            <p className={inter.className}>
+              Find ways to help thousans of people who are affected.
+            </p>
+          </a>
+
+          <a
+            href="https://www.cnn.com/middleeast/live-news/turkey-earthquake-latest-020623/index.html"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={inter.className}>
+              Learn <span>-&gt;</span>
+            </h2>
+            <p className={inter.className}>
+              Learn about what's going on in Turkey right now.
+            </p>
+          </a>
+
+        </div>
+      </main>
+    </>
+  )
+}
