93f82292b883a6ea9091de6869b93d81865c810d
erben <erben@umich.edu>
2023-02-06 13:45:35 -0500

    update button

--- turkeyearthquakedonation/pages/index.js
+++ turkeyearthquakedonation/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Help Turkey</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
