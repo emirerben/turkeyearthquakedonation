0a8209c50b47b460bad65f724cbd90da821aa627
erben <erben@umich.edu>
2023-02-06 13:47:35 -0500

    update button

--- turkeyearthquakedonation/pages/donate.js
+++ turkeyearthquakedonation/pages/donate.js
@@ -12,7 +12,7 @@ export default function Home() {
       <Head>
         <title>Donations for Earthquake in Turkey</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/turkiye1" />
       </Head>
       <div className={styles.headerStyle}>
         <div className={styles.upperlinksStyle}>
--- turkeyearthquakedonation/pages/index.js
+++ turkeyearthquakedonation/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <Head>
         <title>Help Turkey</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/turkiye1.jpeg" />
       </Head>
       <div className={styles.headerStyle}>
         <div className={styles.upperlinksStyle}>
